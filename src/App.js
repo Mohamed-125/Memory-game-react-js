9cf4199c7b73246f57272959c188a391e5787557
mohamed ibrahim <mohamedibrahim.fed@gmail.com>
2022-10-29 00:07:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,172 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [firstChoice, setFirstChoice] = useState({});
+  const [secondChoice, setSecondChoice] = useState({});
+  const imgs = [
+    {
+      img: "https://seeklogo.com/images/H/html5-logo-EF92D240D7-seeklogo.com.png",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://seeklogo.com/images/C/css-3-logo-AF06D75231-seeklogo.com.png",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://mpng.subpng.com/20190627/ttx/kisspng-javascript-computer-icons-scalable-vector-graphics-list-of-javascript-enhancements-fandom-developers-5d145895b06253.7824611015616144857225.jpg",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://spng.subpng.com/20180815/ta/kisspng-sass-logo-cascading-style-sheets-scalable-vector-g-codzero-cms-blog-tool-publishing-platform-5b74aaa0a0f4b2.3928971215343725126593.jpg",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://seeklogo.com/images/H/html5-logo-EF92D240D7-seeklogo.com.png",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://seeklogo.com/images/C/css-3-logo-AF06D75231-seeklogo.com.png",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://mpng.subpng.com/20190627/ttx/kisspng-javascript-computer-icons-scalable-vector-graphics-list-of-javascript-enhancements-fandom-developers-5d145895b06253.7824611015616144857225.jpg",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://spng.subpng.com/20180815/ta/kisspng-sass-logo-cascading-style-sheets-scalable-vector-g-codzero-cms-blog-tool-publishing-platform-5b74aaa0a0f4b2.3928971215343725126593.jpg",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png",
+      id: Math.random(),
+      matched: false,
+    },
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png",
+      id: Math.random(),
+      matched: false,
+    },
+  ];
+
+  const [cards, setCards] = useState(imgs);
+
+  const suffleHandler = () => {
+    setCards((pre) => {
+      return pre
+        .map((img) => {
+          return { ...img, id: Math.random(), matched: false };
+        })
+        .sort((a, b) => a.id - b.id);
+    });
+    setFirstChoice({});
+    setSecondChoice({});
+  };
+
+  const cardClickHandler = (card) => {
+    if (firstChoice.id) {
+      setSecondChoice({ img: card.img, id: card.id });
+    } else {
+      setFirstChoice({ img: card.img, id: card.id });
+    }
+  };
+
+  useEffect(() => {
+    if (firstChoice?.img && secondChoice?.img) {
+      if (firstChoice.img === secondChoice.img) {
+        setCards((pre) =>
+          pre.map((card) => {
+            if (card.img === firstChoice.img) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          })
+        );
+        setFirstChoice({});
+        setSecondChoice({});
+      } else {
+        Array.from(document.querySelectorAll(".front")).forEach(
+          (f) => (f.style.pointerEvents = "none")
+        );
+        setTimeout(() => {
+          setFirstChoice({});
+          setSecondChoice({});
+          Array.from(document.querySelectorAll(".front")).forEach(
+            (f) => (f.style.pointerEvents = "all")
+          );
+        }, 900);
+      }
+    } else {
+    }
+  }, [firstChoice, secondChoice]);
+
+  useEffect(() => {
+    if (cards.every((card) => card.matched)) {
+      setTimeout(() => {
+        alert("you win");
+      }, 400);
+    }
+  }, [cards]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={suffleHandler}>new game</button>
+      <div className="card-container">
+        {cards.map((card) => {
+          return (
+            <div key={card.id} className="card-div">
+              <div
+                className={`front ${
+                  card.matched
+                    ? "flipped"
+                    : card.id === firstChoice.id
+                    ? "flipped"
+                    : card.id === secondChoice.id
+                    ? "flipped"
+                    : ""
+                }`}
+                onClick={() => {
+                  cardClickHandler(card);
+                }}
+              />
+              <div
+                data-bg-img={card.img}
+                className={`back ${
+                  card.matched
+                    ? "flipped"
+                    : card.id === firstChoice.id
+                    ? "flipped"
+                    : card.id === secondChoice.id
+                    ? "flipped"
+                    : ""
+                }`}
+                style={{
+                  backgroundImage: `url(${card.img})`,
+                }}
+              ></div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
